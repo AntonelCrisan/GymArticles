c53a843b1c0438a212d69b5c2ff7b9b4a951cf9a
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2025-06-03 15:06:48 +0300

    Update addArticle.js

--- public/scripts/addArticle.js
+++ public/scripts/addArticle.js
@@ -16,7 +16,8 @@ form.addEventListener('submit', async (e) => {
         const res = await fetch('/add-article', {
             method: 'POST',
             body: JSON.stringify({image, name, price, category, subcategory, cantity, description}),
-            headers: {'Content-Type': 'application/json'}
+            headers: {'Content-Type': 'application/json'},
+             credentials: 'include'
           });
         const data = await res.json();
         if(data.msg){
