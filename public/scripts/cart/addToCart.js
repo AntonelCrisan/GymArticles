9a15a34111c4763aac7263216cf582a666dd1d87
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2025-06-03 14:18:30 +0300

    Update addToCart.js

--- public/scripts/cart/addToCart.js
+++ public/scripts/cart/addToCart.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: {
                     'Content-Type': 'application/json'
                 },
+                credentials: 'include',
                 body: JSON.stringify({
                     productId: cart.dataset.productId
                 })
